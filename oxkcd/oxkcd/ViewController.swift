a494d2f96f398c957187654f34123cb09da284fb
Sergey Dunets <dunets.devel@gmail.com>
2015-11-12 00:42:38 +0200

    oxkcd-2: add new single view project

---
+++ oxkcd/oxkcd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oxkcd
+//
+//  Created by Sergey Dunets on 11/12/15.
+//  Copyright © 2015 oPenguins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
